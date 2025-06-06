7129919f80db9124efbbbd6e3882d61db9255b18
Daniele Buratti <danibura2007@gmail.com>
2025-06-06 22:32:26 +0200

    Fix

--- config/api.js
+++ config/api.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
-  import.meta.env.MODE === "development"
+  import.meta.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "https://tuo-backend.vercel.app"; // metti il dominio reale del backend
+    : "https://trip-planner-rust-gamma.vercel.app/"; // metti il dominio reale del backend
 
 export default API_BASE_URL;
