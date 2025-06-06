fc6e0382c26334d99fd1924cb88b41f4eb63aeb8
Daniele Buratti <danibura2007@gmail.com>
2025-06-06 22:38:03 +0200

    Fixx

--- config/api.js
+++ config/api.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   import.meta.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "https://trip-planner-rust-gamma.vercel.app/"; // metti il dominio reale del backend
+    : "tripplanner-backend-production.up.railway.app"; // metti il dominio reale del backend
 
 export default API_BASE_URL;
