6189e5af2f36c22a428f5e326585cb89d0b81a15
Daniele Buratti <danibura2007@gmail.com>
2025-06-07 11:40:24 +0200

    Render

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
- try{
-    const conn=await mongoose.connect(process.env.MONGO_URL);
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
- }catch(error){
+  } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
- }
-}
+  }
+};
