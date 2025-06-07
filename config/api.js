be26f368b8d73dcb131ad23547cb3fd28bf43f25
Daniele Buratti <danibura2007@gmail.com>
2025-06-07 11:52:08 +0200

    Render2

--- config/api.js
+++ config/api.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "tripplanner-backend-production.up.railway.app"; // metti il dominio reale del backend
+    : "https://tripplanner-backend-2xgg.onrender.com"; // metti il dominio reale del backend
 
 export default API_BASE_URL;
